c453e4c83e3b692a3217e058212532d13cd01d15
raviinala <167518405+raviinala@users.noreply.github.com>
2025-02-20 15:28:05 +0530

    Update terra-cloud.tf

--- terra-cloud.tf
+++ terra-cloud.tf
@@ -1,8 +1,8 @@
-# provider "azurerm" {
-#  subscription_id = "33ab2735-3851-4bf4-9407-8979233f8841"
-#     features {}
-# }
-# resource "azurerm_resource_group" "rg1" {
-#   name     = "rg1"
-#   location = "westus"
-# }
+provider "azurerm" {
+ subscription_id = "33ab2735-3851-4bf4-9407-8979233f8841"
+    features {}
+}
+resource "azurerm_resource_group" "rg1-ri10" {
+  name     = "rg1-ri10"
+  location = "westus"
+}
