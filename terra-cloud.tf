b06ad32a21c3308ee44eb9da3b015e6e4220eda3
raviinala <inala.ravishankar@gmail.com>
2025-02-20 14:13:56 +0530

    first commit

---
+++ terra-cloud.tf
@@ -0,0 +1,8 @@
+# provider "azurerm" {
+#  subscription_id = "33ab2735-3851-4bf4-9407-8979233f8841"
+#     features {}
+# }
+# resource "azurerm_resource_group" "rg1" {
+#   name     = "rg1"
+#   location = "westus"
+# }
